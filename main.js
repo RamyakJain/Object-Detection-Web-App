7647547172861ddddefe095e09ed4bcc7cdf3198
RamyakJain <73877400+RamyakJain@users.noreply.github.com>
2021-01-18 18:28:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+video = "";
+function preload(){
+    video = createVideo('video.mp4');
+    video.hide();
+}
+function setup(){
+    canvas = createCanvas(480,300);
+    canvas.center();
+    canvas.position(400, 250);
+}
+function draw(){
+    image(video,0, 0, 480 ,380);
+}
+function start(){
+    objectDetector = ml5.objectDetector('cocoSSD', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+function modelLoaded(){
+    console.log("CocoSSD Is Initialized");
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
